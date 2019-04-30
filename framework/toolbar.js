e1410d22d43bd85d975142878cea695241f7c20c
colemckinnon <colemckinnon.school@gmail.com>
2019-04-29 22:18:38 -0700

    Setting up framework. Toolbar file created but blank for now.

---
+++ framework/toolbar.js
@@ -0,0 +1,17 @@
+
+function createToolBar(){
+
+    buttongroup = []
+    tools = []
+    const button_size = 25
+    const offset = 4
+
+    return{
+        getSelectedTool: () => {},
+        addNode: () => {},
+        addEdge: () => {},
+    }
+
+}
+
+export default {createToolBar}
