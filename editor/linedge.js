1a8b2608b7ea627ab14e6512dd1f3c7fb60e021e
Ian SooHoo <ian@iansoohoo.me>
2019-05-05 18:56:47 -0700

    splitting files

---
+++ editor/linedge.js
@@ -0,0 +1,97 @@
+function createLineEdge() {
+    let start = undefined
+    let end = undefined
+    let dashed = false
+    let prototype = 'genericEdge'
+    let objectType = 'edge'
+    let startlabel = ''
+    let midlabel = ''
+    let endlabel = ''
+    return {
+        setElementID: (newElementID) => {
+            elementID = newElementID
+        },
+        getStartLabel: () => {
+            return startlabel
+        },
+        setStartLabel: (newlable) => {
+            startlabel = newlable
+        },
+        getMidLabel: () => {
+            return midlabel
+        },
+        setMidLabel: (newlabel) => {
+            midlabel = newlabel
+        },
+        getEndLabel: () => {
+            return endlabel
+        },
+        setEndLabel: (newlabel) => {
+            endlabel = newlabel
+        },
+        getAttributes() {
+            return [
+                this.getStartLabel, this.setStartLabel,
+                this.getMidLabel, this.setMidLabel,
+                this.getEndLabel, this.setEndLabel
+            ]
+        },
+        connect: (s, e) => {
+            start = s
+            end = e
+        },
+        dashed: (boolean) => {
+            dashed = boolean
+            if (dashed) prototype = 'dashedEdge'
+        },
+        getPrototype: () => {
+            return prototype
+        },
+        getObjectType: () => {
+            return objectType
+        },
+        getConnectionPoints: () => {
+            return {
+                x1: center(start.getBounds()).x,
+                y1: center(start.getBounds()).y,
+                x2: center(end.getBounds()).x,
+                y2: center(end.getBounds()).y
+            }
+        },
+        contains: aPoint => {
+            return ptSegDistSq(center(start.getBounds()).x, center(start.getBounds()).y,
+                center(end.getBounds()).x, center(end.getBounds()).y, aPoint.x, aPoint.y) < 4
+        },
+        draw: () => {
+            const canvas = document.getElementById(elementID)
+            const ctx = canvas.getContext('2d')
+            if (dashed) {
+                ctx.setLineDash([4, 4]);
+            }
+            ctx.beginPath()
+            const p = center(start.getBounds()) // Just pick the center of the bounds for now
+            const q = center(end.getBounds()) // Not the "connection points" that graphed2 uses
+            ctx.moveTo(p.x, p.y)
+            ctx.lineTo(q.x, q.y)
+            ctx.stroke()
+
+            // Start and end labels
+            ctx.font = "12px Helvetica";
+            ctx.textAlign = "center";
+
+            startX = p.x + (q.x - p.x) / 3.4
+            startY = p.y + (q.y - p.y) / 3.4 - 4
+            midX = p.x + (q.x - p.x) / 2
+            midY = p.y + (q.y - p.y) / 2 - 4
+            endX = q.x + (p.x - q.x) / 3.4
+            endY = q.y + (p.y - q.y) / 3.4 - 4
+            ctx.fillText(startlabel, startX, startY);
+            ctx.fillText(midlabel, midX, midY);
+            ctx.fillText(endlabel, endX, endY);
+
+        },
+    }
+
+}
+
+module.exports = {createLineEdge}
---
+++ editor/nvpair.js
@@ -0,0 +1,105 @@
+function createNVPair() {
+    let x = 100
+    let y = 100
+    let size = 120
+    let width = 0
+    let height = 0
+    let name = 'Name'
+    let value = 'Value'
+    let elementID = undefined
+    let prototype = 'NVpair'
+    let objectType = 'node'
+    return {
+        setElementID: (newID) => {
+            elementID = newID
+        },
+        getName: () => {
+            return name
+        },
+        setName: (newName) => {
+            name = newName
+        },
+        getValue: () => {
+            return value
+        },
+        setValue: (newValue) => {
+            value = newValue
+        },
+        getBounds: () => {
+            return {
+                x: x,
+                y: y,
+                width: width,
+                height: height
+            }
+        },
+        getAttributes() {
+            return [this.getName, this.setName,
+            this.getValue, this.setValue]
+        },
+        contains: p => {
+            return (Math.abs(x + width / 2 - p.x) < width / 2 && Math.abs(y + height / 2 - p.y) < height / 2)
+        },
+        translate: (dx, dy) => {
+            x += dx
+            y += dy
+        },
+        getPrototype: () => {
+            return prototype
+        },
+        getObjectType: () => {
+            return objectType
+        },
+        draw: () => {
+            let body = document.getElementById(elementID)
+            let table = document.createElement('table')
+            table.style.fontSize = 5
+            table.style.position = 'absolute'
+            table.style.backgroundColor = 'white'
+            table.style.left = 5
+            table.style.top = 5
+            table.width = 10
+
+            let tableBody = document.createElement('tbody')
+            table.appendChild(tableBody)
+
+            let tr = document.createElement('tr')
+            let th = document.createElement('th')
+            th.innerText = name + ' = ' + value
+            tr.appendChild(th)
+            tableBody.appendChild(tr)
+
+            body.appendChild(table)
+
+        },
+
+        drawInCanvas: () => {
+            let body = document.getElementById(elementID)
+            let table = document.createElement('table')
+            table.style.fontSize = 12
+            table.style.backgroundColor = 'lightgray'
+            table.style.left = 5
+            table.style.top = 5
+            table.width = size
+
+            let tableBody = document.createElement('tbody')
+            table.appendChild(tableBody)
+
+            let tr = document.createElement('tr')
+            let th = document.createElement('th')
+            th.innerText = name + ' = ' + value
+            tr.appendChild(th)
+            tableBody.appendChild(tr)
+            body.appendChild(table)
+            tr.offsetWidth = width
+            th.offsetWidth = width
+
+            //mark position of the nvpair
+            let bounds = body.getBoundingClientRect()
+            x = bounds.x - 3
+            y = bounds.y - 73
+            width = bounds.width
+            height = bounds.height
+        }
+    }
+}
